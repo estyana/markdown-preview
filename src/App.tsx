c40fd48d6af82001b443ea1a73336cdfe8b7c766
github_username <you@domain.com>
2024-01-21 20:57:24 +0700

    Kedua Kali

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import './App.css'
+import ReactMarkdown from "react-markdown"
+
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+//this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine =='\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **hold**... whoa!
+Or _italic_.
+Or... **_both_**
+
+There's also [links](https://www.freecodecamp.com), and 
+> Block Quotes!
+
+![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg)
+
+- And of course there are list.
+  - Some are bullied.
+    - With different indentation levels.
+      - That look like this.
+`
+
+function App() {
+  const [markdownText, setMarkdownText] = useState<string>(defaultMarkdown)
+
+
+  return (
+    <>
+      <div>
+        <h1 style ={{ textAlign: "center" }}>Markdown Previewer</h1>
+        <div className="boxes-container">
+          <textarea name="editor" id="editor" value={markdownText} onChange={(e) => setMarkdownText(e.target.value)}></textarea>
+          <div id="preview">
+            <ReactMarkdown>{markdownText}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
